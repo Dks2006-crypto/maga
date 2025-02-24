22aa714adf67f9b0a640b0425dcb8aea4aa33750
Dks2006-crypto <denis18kpina@gmail.com>
2025-02-24 15:55:16 +0300

    Сделал кнопку для перехода на страницу всех продуктов

--- resources/views/components/app/layout.blade.php
+++ resources/views/components/app/layout.blade.php
@@ -6,9 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Название страницы' }} - {{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .flex{
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            align-items: center;
+            justify-content: center;
+        }
+        .link{
+            text-decoration: none;
+            background-color: rgb(37, 99, 235);
+            border-radius: 12px;
+            color: white;
+            font-size: 20px;
+            padding: 6px;
+        }
+    </style>
 </head>
 <body>
+    @include('components.partials.header')
     {{ $slot }}
+    @include('components.partials.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/page/brand-product.blade.php
+++ resources/views/page/brand-product.blade.php
@@ -1,5 +1,8 @@
 <x-app.layout>
-    <x-slot:title>{{ $titlePage }}</x-slot:title>
-    <h1>Это страница записей определенного бренда</h1>
-    <livewire:product-list :products="$products" />
+    <div class="flex">
+        <x-slot:title>{{ $titlePage }}</x-slot:title>
+        <h1>Это страница записей определенного бренда</h1>
+        <livewire:product-list :products="$products" />
+        <a href="{{route('product.index')}}" class="link">Все продукты</a>
+    </div>
 </x-app.layout>
